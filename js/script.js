ca1855027eb365ab1cdf9c91ef95d306c2f2ab95
João Paulo <paulo.j7.jp89@gmail.com>
2023-03-29 05:22:29 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const search = document.querySelector('.search')
+
+const pokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemons = () => Array(150).fill().map((_, index) => 
+        fetch(pokemonUrl(index + 1))
+            .then(response => response.json()))
+
+const generateHTML = pokemons => pokemons.reduce((acumulador, pokemon) => {
+    const types = pokemon.types
+        .map(typeInfo => typeInfo.type.name)
+    acumulador += `
+      <li class="card ${types[0]}">
+         <img class="card-image" 
+            alt="${pokemon.name}" 
+            src="https://raw.githubusercontent.com/RafaelSilva2k22/PokemonImages/main/images/${pokemon.id}.png"/>
+         <h2 class="card-title">${pokemon.id}º ${pokemon.name}</h2>
+         <p class="card-subtitle">${types.join(' | ')}</p>
+      </li>
+    `
+    return acumulador
+}, '')
+
+
+const insertPokemonsHTML = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
+}
+
+const pokemonPromises = generatePokemons()
+
+const eventSearch = () => {
+    const pokeNames = document.querySelectorAll('.card-title')
+    const searchInput = search.value.toLowerCase()
+
+    pokeNames.forEach((pokeName => {
+        pokeName.parentElement.style.display = 'block'
+
+        if(!pokeName.innerHTML.toLowerCase().includes(searchInput)) {
+            pokeName.parentElement.style.display = 'none'
+        }
+    }))
+}
+
+search.addEventListener('input', eventSearch)
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonsHTML)
